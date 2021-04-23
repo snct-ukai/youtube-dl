0d17561c79700792f9ff8dfec68cad25937b8a93
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-04-23 22:31:30 +0900

    プロジェクト ファイルを追加します。

---
+++ youtube-dl/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace youtube_dl
+{
+    public partial class Form1 : Form
+    {
+        string fpath,url;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Text = "ダウンロード";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            fpath = fbd.SelectedPath;
+            label1.Text = fpath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fpath != null && url != null)
+            {
+                var youtube = YouTube.Default;
+                var video = youtube.GetVideo(url);
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.FileName = video.FullName;
+                try
+                {
+                    System.IO.File.WriteAllBytes(fpath +"/"+ sfd.FileName, video.GetBytes());
+                    MessageBox.Show("ダウンロード完了", "完了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            url = textBox1.Text;
+        }
+    }
+}
